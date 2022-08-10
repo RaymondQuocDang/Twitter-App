0ed4d454e14566faba1b243a9d5116d1ca763996
Raymond Dang <raymondquocdang@gmail.com>
2022-08-10 18:27:24 -0400

    First implementation of favorite accounts

--- twitter-app/src/components/FavoriteAccounts.js
+++ twitter-app/src/components/FavoriteAccounts.js
@@ -1,43 +1,65 @@
 import './FavoriteAccounts.css';
+import retweetSymbol from './images/retweet.png'
+import twitterProfilePic from './images/TwitterProfilePic.png'
 
 function FavoriteAccounts() {
     return(
-        <div>
-            <div className='favorite-accounts'>
-                <p>Profile 1</p>
-                <p>likes</p>
-                <p>date</p>
-                <p>profile picture</p>
-                <p>username</p>
+        <div className='favorite-accounts-page'>
+            <div className='header-container-fa'>
+            </div>    
+            <div className='account-summary-tweet-container'>
+                <div className='account-summary-container'>
+                    <div className='account-summary-box'>
+                        <img className='account-summary-pic' src={twitterProfilePic} alt='Default twitter profile pic'></img>
+                        <div><p className='account-summary-text'>What it do baby!</p></div>
+                    </div>
+                </div>
+                <div className='tweet-wrapper-fa'>
+                    <div className='tweet-flex-container-fa'>
+                        <div className='pp-flex-container'>
+                            <div><img className='profile-pic' src={twitterProfilePic} alt='Default twitter profile pic'></img></div>
+                        </div>
+                        <div className='content-container'> 
+                            <div className='username'>Username</div>
+                            <div className='text'>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</div>
+                            <div className='media'>Media</div>
+                            <div className='retweet-likes-container'>
+                                <div className='retweets-container'>
+                                    <img className='retweet-symbol' src={retweetSymbol} alt='Retweet symbol'></img>
+                                    <p>0</p>    
+                                </div>
+                                <div className='likes'>❤ 0</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='favorite-accounts'>
-                <p>Profile 2</p>
-                <p>likes</p>
-                <p>date</p>
-                <p>profile picture</p>
-                <p>username</p>
+
+            <div className='account-summary-tweet-container'>
+                <div className='tweet-wrapper-fa'>
+                    <div className='tweet-flex-container-fa'>
+                        <div className='pp-flex-container'>
+                            <div><img className='profile-pic' src={twitterProfilePic} alt='Default twitter profile pic'></img></div>
+                        </div>
+                        <div className='content-container'> 
+                            <div className='username'>Username</div>
+                            <div className='text'>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</div>
+                            <div className='media'>Media</div>
+                            <div className='retweet-likes-container'>
+                                <div className='retweets-container'>
+                                    <img className='retweet-symbol' src={retweetSymbol} alt='Retweet symbol'></img>
+                                    <p>0</p>    
+                                </div>
+                                <div className='likes'>❤ 0</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='account-summary-container'>
+                    <div className='account-summary-box'></div>
+                </div>
             </div>
-            <div className='favorite-accounts'>
-                <p>Profile 3</p>
-                <p>likes</p>
-                <p>date</p>
-                <p>profile picture</p>
-                <p>username</p>
-            </div>
-            <div className='favorite-accounts'>
-                <p>Profile 4</p>
-                <p>likes</p>
-                <p>date</p>
-                <p>profile picture</p>
-                <p>username</p>
-            </div>
-            <div className='favorite-accounts'>
-                <p>Profile 5</p>
-                <p>likes</p>
-                <p>date</p>
-                <p>profile picture</p>
-                <p>username</p>
-            </div>
+
         </div>
     );
 }
--- twitter-app/src/components/SearchPage.js
+++ twitter-app/src/components/SearchPage.js
@@ -11,12 +11,12 @@ function SearchPage() {
                     <input type="text" className="input-search" placeholder="Type to Search..." />
                 </div>
             </div>
-            <div className='profile-wrapper'>
+            <div className='tweet-wrapper'>
                 <div className='tweet-flex-container'>
                     <div className='pp-flex-container'>
-                        <div><img className='profile-pic' src={twitterProfilePic}></img></div>
+                        <div><img className='profile-pic' src={twitterProfilePic} alt='Twitter default profile pic'></img></div>
                     </div>
-                    <div className='content-flex-container'> 
+                    <div className='content-container'> 
                         <div className='username'>Username</div>
                         <div className='text'>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</div>
                         <div className='media'>Media</div>
