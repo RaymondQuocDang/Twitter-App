d5ee7ef817133a4c33b1f653871f0468bbe2c490
Raymond Dang <raymondquocdang@gmail.com>
2022-08-12 15:11:40 -0400

    Added some server side functionality

--- twitter_flask_app/app.py
+++ twitter_flask_app/app.py
@@ -1,9 +1,30 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+import requests
+from requests.structures import CaseInsensitiveDict
 
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/')
+url = "https://api.twitter.com/1.1/search/tweets.json?q=tesla&result_type=popular&count=10&tweet_mode=extended"
+
+headers = CaseInsensitiveDict()
+headers["Accept"] = "application/json"
+headers["Authorization"] = "Bearer AAAAAAAAAAAAAAAAAAAAAP5tfwEAAAAA44hCCBXctkQ6x%2FznD4X1l98X2oo%3D41z2gIQv7qgOJZ2TPJtYVS5y1EVJp6Y95KSCXEj30ZNfiUnFnv"
+
+
+resp = requests.get(url, headers=headers)
+
+data = resp.json()['statuses']
+
+for i in range(10):
+    print(data[i]['user']['name'])
+    print(data[i]['retweet_count'])
+    print(data[i]['favorite_count'])
+    print(data[i]['full_text'])
+    print('')
+
 def index():
+
+
     return '<h1>Hello</h1>'
