0786c16142cc1e47fcb309ba3ec5d2eef8337ceb
Raymond Dang <raymondquocdang@gmail.com>
2022-08-29 11:33:49 -0400

    added default header and hid bearer token

--- twitter_flask_app/app.py
+++ twitter_flask_app/app.py
@@ -1,11 +1,17 @@
+from email.policy import default
 from re import search
 from flask import Flask, request, send_from_directory
 import requests
 from requests.structures import CaseInsensitiveDict
 from flask_cors import CORS, cross_origin
+import os
 
 app = Flask(__name__, static_folder='twitter-react-app/build', static_url_path='')
 cors = CORS(app)
+
+default_header = CaseInsensitiveDict()
+default_header["Accept"] = "application/json"
+default_header["Authorization"] = "Bearer " + os.environ['TOKEN']
 
 @app.route('/tweets')
 @cross_origin()
@@ -15,11 +21,7 @@ def search_tweets():
 
     url = f"https://api.twitter.com/1.1/search/tweets.json?q={search_params}&result_type=popular&count=10&tweet_mode=extended"
 
-    headers = CaseInsensitiveDict()
-    headers["Accept"] = "application/json"
-    headers["Authorization"] = "Bearer AAAAAAAAAAAAAAAAAAAAAP5tfwEAAAAA44hCCBXctkQ6x%2FznD4X1l98X2oo%3D41z2gIQv7qgOJZ2TPJtYVS5y1EVJp6Y95KSCXEj30ZNfiUnFnv"
-
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=default_header)
     search_results = resp.json()['statuses']
 
     return {"search_results": search_results, "param": search_params}
@@ -34,11 +36,7 @@ def search_users_tweets():
 
     url = f"https://api.twitter.com/1.1/search/tweets.json?q=(from%3A{search_params})&src=typed_query&f=live&count=10&tweet_mode=extended"
 
-    headers = CaseInsensitiveDict()
-    headers["Accept"] = "application/json"
-    headers["Authorization"] = "Bearer AAAAAAAAAAAAAAAAAAAAAP5tfwEAAAAA44hCCBXctkQ6x%2FznD4X1l98X2oo%3D41z2gIQv7qgOJZ2TPJtYVS5y1EVJp6Y95KSCXEj30ZNfiUnFnv"
-
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=default_header)
     search_results = resp.json()['statuses']
 
     return {"search_results": search_results, "param": search_params}
@@ -49,38 +47,23 @@ def search_users_tweets():
 def search_favorite_accounts_tweets():
 
     url = f"https://api.twitter.com/1.1/search/tweets.json?q=(from%3Anintendoamerica)&count=100&tweet_mode=extended"
-    headers = CaseInsensitiveDict()
-    headers["Accept"] = "application/json"
-    headers["Authorization"] = "Bearer AAAAAAAAAAAAAAAAAAAAAP5tfwEAAAAA44hCCBXctkQ6x%2FznD4X1l98X2oo%3D41z2gIQv7qgOJZ2TPJtYVS5y1EVJp6Y95KSCXEj30ZNfiUnFnv"
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=default_header)
     nintendo_tweet_list = resp.json()['statuses']
 
     url = f"https://api.twitter.com/1.1/search/tweets.json?q=(from%3AMarvel)&count=100&tweet_mode=extended"
-    headers = CaseInsensitiveDict()
-    headers["Accept"] = "application/json"
-    headers["Authorization"] = "Bearer AAAAAAAAAAAAAAAAAAAAAP5tfwEAAAAA44hCCBXctkQ6x%2FznD4X1l98X2oo%3D41z2gIQv7qgOJZ2TPJtYVS5y1EVJp6Y95KSCXEj30ZNfiUnFnv"
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=default_header)
     marvel_tweet_list = resp.json()['statuses']
 
     url = f"https://api.twitter.com/1.1/search/tweets.json?q=(from%3ANBA)&count=100&tweet_mode=extended"
-    headers = CaseInsensitiveDict()
-    headers["Accept"] = "application/json"
-    headers["Authorization"] = "Bearer AAAAAAAAAAAAAAAAAAAAAP5tfwEAAAAA44hCCBXctkQ6x%2FznD4X1l98X2oo%3D41z2gIQv7qgOJZ2TPJtYVS5y1EVJp6Y95KSCXEj30ZNfiUnFnv"
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=default_header)
     nba_tweet_list = resp.json()['statuses']
 
     url = f"https://api.twitter.com/1.1/search/tweets.json?q=(from%3AOlympics)&count=100&tweet_mode=extended"
-    headers = CaseInsensitiveDict()
-    headers["Accept"] = "application/json"
-    headers["Authorization"] = "Bearer AAAAAAAAAAAAAAAAAAAAAP5tfwEAAAAA44hCCBXctkQ6x%2FznD4X1l98X2oo%3D41z2gIQv7qgOJZ2TPJtYVS5y1EVJp6Y95KSCXEj30ZNfiUnFnv"
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=default_header)
     olympics_tweet_list = resp.json()['statuses']
 
     url = f"https://api.twitter.com/1.1/search/tweets.json?q=(from%3APokemon)&count=100&tweet_mode=extended"
-    headers = CaseInsensitiveDict()
-    headers["Accept"] = "application/json"
-    headers["Authorization"] = "Bearer AAAAAAAAAAAAAAAAAAAAAP5tfwEAAAAA44hCCBXctkQ6x%2FznD4X1l98X2oo%3D41z2gIQv7qgOJZ2TPJtYVS5y1EVJp6Y95KSCXEj30ZNfiUnFnv"
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=default_header)
     pokemon_tweet_list = resp.json()['statuses']
 
     return {
